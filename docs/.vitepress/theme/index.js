abbaa57e409c703a4808771d09df5da8dd7e1cf7
zhao-farmer <857899180@qq.com>
2025-04-27 11:51:33 +0800

    更新一些链接

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -2,14 +2,10 @@ import DefaultTheme from 'vitepress/theme'
 // 导入设置的样式
 import './custom.css'
 
-// import { Fancybox } from '@fancyapps/ui';
-// import * as pkg from '@fancyapps/ui';
-// const { Fancybox } = pkg;
-import { Fancybox } from '@fancyapps/ui/types/index.d.ts';
-
+import Fancyapps from "@fancyapps/ui/dist/fancybox/fancybox.umd";
 import '@fancyapps/ui/dist/fancybox/fancybox.css'
 
-Fancybox.bind('[data-fancybox="gallery"]');
+Fancyapps.Fancybox.bind('[data-fancybox="gallery"]');
 
 
 export default{
